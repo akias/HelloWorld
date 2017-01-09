99f74ca1c8a1d5e28053e9dcc15e56d5605e0b3f
akias <saikam5yday@gmail.com>
2017-01-09 22:00:57 +0900

    the first

---
+++ src/helloworld.java
@@ -0,0 +1,13 @@
+
+public class helloworld {
+	public static int x = 0;
+	public static void main(String[] args){
+		System.out.println("hello world!");
+		add();
+	System.out.println(x);
+		
+	}
+	public static void add(){
+		x = x+1;
+	}
+}
